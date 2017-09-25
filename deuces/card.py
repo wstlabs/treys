46329cf7af0021676195cdb3c9460c5549ffc213
wstlabs <notyet@notyet.org>
2017-09-24 23:40:01 -0400

    prefer tidier form of pretty-printing

--- deuces/card.py
+++ deuces/card.py
@@ -189,7 +189,7 @@ class Card ():
 
         r = Card.STR_RANKS[rank_int]
 
-        return " [ " +str(r)+ " " +str(s)+ " ] "
+        return str(r)+str(s)
 
     @staticmethod
     def print_pretty_card(card_int):
