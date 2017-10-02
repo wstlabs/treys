dcac97e94095715d8a6c57735a2c7a8c32f65808
wstlabs <notyet@notyet.org>
2017-10-01 20:51:27 -0400

    dict => OrderedDict

--- treys/card.py
+++ treys/card.py
@@ -44,7 +44,7 @@ class Card ():
     STR_RANKS = '23456789TJQKA'
 
     # converstion from string => int
-    CHAR_RANK_TO_INT_RANK = dict(zip(STR_RANKS, range(0,13)))
+    CHAR_RANK_TO_INT_RANK = OrderedDict(zip(STR_RANKS, range(0,13)))
     CHAR_SUIT_TO_INT_SUIT = OrderedDict(zip('shdc',[1,2,4,8]))
     INT_SUIT_TO_CHAR_SUIT = 'xshxdxxxc'
 
